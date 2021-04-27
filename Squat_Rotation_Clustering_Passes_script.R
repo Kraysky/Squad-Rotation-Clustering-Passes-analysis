8bbb81e76d4c7a9bb7ba727b3aafce601091e6a2
Patryk Krajewski <pkrajewski@pepco.eu>
2021-04-27 21:22:45 +0200

    Third commit

--- Squat_Rotation_Clustering_Passes_script.R
+++ Squat_Rotation_Clustering_Passes_script.R
@@ -1,5 +1,11 @@
-#loading neceessary libraries
-
+#loading neccessary libraries
 library(rjson)
 library(data.table)
 
+#load data from JSON file into a list
+competitions <- fromJSON(file = "C:\\Users\\pkrajewski\\Desktop\\data\\competitions.json")
+
+#converting list into dataframe
+competitions.df <- data.frame(do.call(rbind,competitions),stringsAsFactors = FALSE)
+
+View(competitions.df)
