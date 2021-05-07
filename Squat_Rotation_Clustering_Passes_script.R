3d93d05f748877578b5fc0b7e37a7e1af40d90e3
Patryk Krajewski <pkrajewski@pepco.eu>
2021-05-07 19:04:08 +0200

    Adding more data

--- Squat_Rotation_Clustering_Passes_script.R
+++ Squat_Rotation_Clustering_Passes_script.R
@@ -36,3 +36,32 @@ all.matches.clean$match_week <- as.numeric(all.matches.clean$match_week)
 all.matches.clean$home_score <- as.numeric(all.matches.clean$home_score)
 all.matches.clean$away_score <- as.numeric(all.matches.clean$away_score)
 
+
+###Obtain Events###
+event.files <- list.files(path="C:\\Users\\pkrajewski\\Desktop\\data\\events",
+                          full.names = TRUE, recursive = TRUE)
+
+event.list <- list()
+for(i in 1:length(event.files)){
+  event.temp <- fromJSON(file = event.files[i])
+  
+  #get unique id's of teams participating in a match
+  teamids <- c()
+  
+  #obtain index where we find the event that talks about Starting XI
+  starting.x11.index <- which(unlist(lapply(event.temp, function(x) x$type$name)) == "Starting XI")
+  starting.x11.list <- list()
+  for(s in 1:2){
+    starting.x11.team1 <- data.frame(matrix(t(unlist(event.temp[[s]]$tactics$lineup)),ncol=5, byrow=TRUE),stringsAsFactors = FALSE)
+    colnames(starting.x11.team1) <- names(unlist(event.temp[[s]]$tactics$lineup))[1:5]
+    starting.x11.team1$formation <- event.temp[[s]]$tactics$formation
+    starting.x11.team1$team_id <- event.temp[[s]]$team$id
+    
+    teamids <- c(teamids, event.temp[[s]]$team$id)
+    
+    starting.x11.team1$team_name <- event.temp[[s]]$team$name
+    starting.x11.list[[s]] <- starting.x11.team1
+  }
+}
+
+
